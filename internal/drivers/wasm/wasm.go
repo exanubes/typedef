aa11f048108f825278ae40d444702b589b6945eb
Exanubes <blog@exanubes.com>
2025-12-23 07:47:16 +0100

    Implement wasm driver

--- internal/drivers/wasm/wasm.go
+++ internal/drivers/wasm/wasm.go
@@ -5,12 +5,35 @@ import "github.com/exanubes/typedef/internal/rpc"
 func Start(input rpc.JSONRPCRequest) rpc.JSONRPCResponse {
 	rpc_router := rpc.NewRouter()
 
-	// TODO:
-	// * marshall input string/byte slice into rpc.JSONRPCRequest
-	// * get method handler from router
-	// * run handler with rpc.JSONRPCRequest.Params
-	// * create rpc.JSONRPCResponse
-	// * return
+	handler, exists := rpc_router.Get(input.Method)
 
-	return rpc.JSONRPCResponse{}
+	if !exists {
+		return rpc.JSONRPCResponse{
+			Version: "2.0",
+			ID:      input.ID,
+			Error: &rpc.RPCError{
+				Code:    rpc.MethodNotFound,
+				Message: "Method not found",
+			},
+		}
+	}
+
+	response, err := handler(input.ID, input.Params)
+
+	if err != nil {
+		return rpc.JSONRPCResponse{
+			Version: "2.0",
+			ID:      input.ID,
+			Error: &rpc.RPCError{
+				Code:    rpc.InvalidRequest,
+				Message: err.Error(),
+			},
+		}
+	}
+
+	return rpc.JSONRPCResponse{
+		Version: "2.0",
+		ID:      input.ID,
+		Result:  response,
+	}
 }
